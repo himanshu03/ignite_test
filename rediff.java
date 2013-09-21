c91679e82095dd8851089ad7c9430739025ef488
himanshu03 <himanshu@panalinks.com>
2013-09-21 03:09:31 -0700

    Committed rediff.java.

--- rediff.java
+++ rediff.java
@@ -1,31 +1,27 @@
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
 
-
-public class test_001 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		WebDriver driver = new FirefoxDriver();
-		//driver.manage().timeouts().implicitlyWait(8L, TimeUnit.SECONDS);
-		driver.get("http://www.rediff.com");
-		driver.findElement(By.xpath("//*[@id='signin_info']/a[1]")).click();
-		driver.findElement(By.xpath("//*[@id='btn_login']")).click();
-		
-		
-		Alert alert = driver.switchTo().alert();
-		alert.getText();
-		alert.accept();
-		driver.close();
-
-	}
-
+public class rediff {
+    public static void main(String[] args) throws Exception {
+        FirefoxDriver wd;
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("https://itunes.apple.com/us/app/sauce/id564308477?mt=12&ign-mpt=uo%3D4");
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 }
